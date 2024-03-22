20438f4b2b6d2f103e158604c10d4db0e3958d02
abimon <eddyabby59@gmail.com>
2024-03-22 16:35:47 +0300

    middleware

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/products', function () {
     $gals = [['image' => '1ds', 'title' => '1 Day Chicks'], ['title' => "1 week rainbow rooster", 'image' => '1wrr'], ['title' => "2 week Sasso", 'image' => '2wsasso'], ['title' => "3 Week", 'image' => '3w'], ['title' => "4 week sasso", 'image' => '4w'], ['title' => "4 week old rainbow rooster", 'image' => '4wrr'], ['title' => "5 week old Sasso", 'image' => '5ws'], ['title' => "KARLO chicks", 'image' => 'kalro'],['title'=>'Brooder','image'=>'brooder'],['title'=>'Harchery','image'=>'harchery'],['title'=>'Packaging','image'=>'pack']];
     return view('products',compact('gals'));
 });
-Route::middleware('auth')->group(function(){
+Route::middleware('auth','isAdmin')->group(function(){
     Route::prefix('admin')->group(function(){
         Route::get('/',[HomeController::class,'dashboard']);
     });
